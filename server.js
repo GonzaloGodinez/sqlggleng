95d4fbdf8154905d7a636b87abbbd10cb34be454
GonzaloGodinez <Gonzalo.Godinez@comcast.net>
2023-11-11 17:02:47 -0700

    created database and insert values into three tables

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Import and require mysql2
+const inquirer = require("inquirer")
 const mysql = require('mysql2');
+require("dotenv").config()
 
 // Connect to database
 const db = mysql.createConnection(
@@ -8,18 +10,81 @@ const db = mysql.createConnection(
     // MySQL username,
     user: 'root',
     // MySQL Password
-    password: '',
+    password: process.env.DB_PW,
     database: 'ggldesign_db'
   },
   console.log(`Connected to the ggldesign_db database.`)
 );
-
+userinput()
 // QUery database using count favorite_books group by in_Stock
-db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-  console.log(results);
-});
+// db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
+//  console.log(results);
+// });
 
 // QUery database using sum(), max(), MIN() AVG  favorite_books group by sections
-db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-  console.log(results);
-});
+// db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
+// console.log(results);
+// });
+
+function userinput() {
+  inquirer.prompt({
+    type: "list",
+    name: "Menu",
+    message: "What would you like to enter?",
+    choices: [
+      "View all department",
+      "View all employees",
+      "View all roles",
+      "add a department",
+      "add role",
+      "add employee",
+      "update employee role"
+    ]
+
+  }).then((answer) => {
+    if (answer.Menu === "View all department") {
+      viewDepartment()
+    }
+    else if (answer.Menu === "View all employees") {
+      ViewallEmployees()
+    }
+    else if (answer.Menu === "View all roles") {
+      ViewallRoles()
+    }
+  })
+}
+
+function viewDepartment() {
+  db.query('SELECT * FROM department', function (err, results) {
+    console.table(results);
+    userinput();
+  });
+}
+function ViewallEmployees() {
+  db.query('SELECT * FROM employee', function (err, results) {
+    console.table(results);
+    userinput();
+  });
+}
+function ViewallRoles() {
+  db.query('SELECT * FROM role', function (err, results) {
+    console.table(results);
+    userinput();
+  });
+}
+function AddDepartment() {
+  inquirer.prompt(
+    {
+      type: "input",
+      name: "name",
+      message: "please enter department name",
+    }
+  ) .then((answer) => {
+    db.query('INSERT INTO department (name) VALUE (?)', [
+      answer.name
+    ], function (err, results) {
+      console.table(results);
+      userinput();
+    });
+  })
+}
